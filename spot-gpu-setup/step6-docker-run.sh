a78eeefd7d409181652f9769f4173c80d44724ce
david mar <david.bryan.mar@gmail.com>
2025-05-04 23:17:27 +0000

    updates to run the stable version

--- spot-gpu-setup/step6-docker-run.sh
+++ spot-gpu-setup/step6-docker-run.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/bash
-docker pull davidbmar/whisperx:no_model
+#docker pull davidbmar/whisperx:no_model
 
 #get the audiofile.
 wget https://2025-03-15-youtube-transcripts.s3.us-east-2.amazonaws.com/audio.mp3
 wget https://2025-03-15-youtube-transcripts.s3.us-east-2.amazonaws.com/test.webm
 
+#docker run --gpus all -it -v "$(pwd):/app" davidbmar/whisperx:no_model -- --model tiny --language en --output_format srt audio.mp3
 
-docker run --gpus all -it -v "$(pwd):/app" davidbmar/whisperx:no_model -- --model tiny --language en --output_format srt audio.mp3
+# 1. Pull the stable base-en image
+docker pull ghcr.io/jim60105/whisperx:base-en
 
+# 2. Run WhisperX on 'audio.mp3' and output SRT subtitles to your current directory
+docker run --gpus all -it \
+  -v "$(pwd):/app" \
+  --rm \
+  ghcr.io/jim60105/whisperx:base-en \
+  -- --output_format srt /app/audio.mp3
 
 
